2433497ec030998b332cf784631a517947281baa
Aaron Hammer-Huber <hamhub7@yahoo.com>
2020-01-08 01:31:38 -0500

    Added ability to input values in terminal and graph support

--- DupingCurve.cpp
+++ DupingCurve.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 #include <iomanip>
+#include <string>
+#include "matplotlibcpp.h"
+
+namespace plt = matplotlibcpp;
 
 long int inventory;
 long int ground;
@@ -7,10 +11,10 @@ int totalTime;
 int numCycles;
 int numLoads;
 
-const int timeToSetup = 3;
-const int timeToDupe = 1;
-const long int targetValue = 2147484000;
-const int startingValue = 9;
+int timeToSetup = 3;
+int timeToDupe = 1;
+long long int targetValue = 2147484000;
+int startingValue = 9;
 
 // Takes 3 seconds
 void setup()
@@ -30,9 +34,21 @@ void dupe()
 
 int main()
 {
-    std::cout << "Dupes |      Final |  Time | Items/sec | Loops | Loads" << std::endl;
+    std::cout << "How long do you take, on average, to do a setup? (Pick items up off floor, drop inventory, QS/QL)" << std::endl;
+    std::cin >> timeToSetup;
+    std::cout << "How long do you take, on average, to do a dupe? (Pick items up off floor, QS/QL)" << std::endl;
+    std::cin >> timeToDupe;
+    std::cout << "What is the target number of items you want to have at the end of the dupe sequence?" << std::endl;
+    std::cin >> targetValue;
+    std::cout << "How many items are you starting with on the ground at the beginning?" << std::endl;
+    std::cin >> startingValue;
 
-    for(int numDupes = 1; numDupes < 100; numDupes++)
+
+    std::cout << "Dupes b/w Setup |      Final |  Time | Items/sec | Loops | Loads" << std::endl;
+
+    const int numLoops = 100;
+    std::vector<double> x(numLoops), y(numLoops);
+    for(int numDupes = 1; numDupes < numLoops; numDupes++)
     {
         inventory = 0;
         ground = startingValue;
@@ -64,13 +80,22 @@ int main()
             }
         }
 
-        std::cout << std::setw(5) << std::setfill(' ') << numDupes << " | ";
+        x.at(numDupes) = numDupes;
+        y.at(numDupes) = totalTime;
+
+        std::cout << std::setw(15) << std::setfill(' ') << numDupes << " | ";
         std::cout << std::setw(10) << std::setfill(' ') << inventory << " | ";
         std::cout << std::setw(5) << std::setfill(' ') << totalTime << " | ";
         std::cout << std::setw(9) << std::setfill(' ') << inventory / totalTime << " | "; 
         std::cout << std::setw(5) << std::setfill(' ') << numCycles << " | ";
         std::cout << std::setw(5) << std::setfill(' ') << numLoads << std::endl;
     }
+
+    plt::figure_size(1200, 700);
+    plt::plot(x, y);
+    plt::xlim(1, numLoops - 1);
+    plt::title("Time vs Number of Dupes");
+    plt::save("graph.png"); 
 
     return 0;
 }
